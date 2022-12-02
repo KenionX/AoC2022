4027e1e6e5f6d0d774a20b2661d746d7e2ceb66c
Kenion <zerrayX@gmail.com>
2022-12-02 08:47:25 +0100

    Day 1 & Day 2

---
+++ Day1/Program.cs
@@ -0,0 +1,3 @@
+﻿var data = File.ReadAllText("Input.txt").Split("\r\n\r\n").Select(elf => elf.Split("\r\n").Sum(int.Parse)).ToArray();
+Console.WriteLine($"Solution1: {data.Max()}");
+Console.WriteLine($"Solution2: {data.OrderDescending().Take(3).Sum()}");
---
+++ Day2/Program.cs
@@ -0,0 +1,15 @@
+﻿var data = File.ReadLines("Input.txt").Select(x => (x[0] - 'A',x[2] - 'X')).ToArray();
+var solution1 = data.Select(x =>
+{
+    var (a, b) = x;
+    return a + 1 + (b - a + 4) % 3 * 3;
+}).Sum();
+
+var solution2 = data.Select(x =>
+{
+    var (a, outcome) = x;
+    return (outcome % 3 * 3) + ((outcome + 2) % 3 + a) % 3 + 1;
+}).Sum();
+
+Console.WriteLine($"Answer1: {solution1}");
+Console.WriteLine($"Answer2: {solution2}");
