f5331d6653ffa3911c8785781e9c2e92965630fd
Kenion <zerrayX@gmail.com>
2022-12-03 13:58:28 +0100

    Updated with aggregate

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -2,7 +2,7 @@
 var components = data.Select(i => i[..(i.Length/2)].Intersect(i[(i.Length/2)..]).First()).ToArray();
 var groupData = Enumerable.Range(0, data.Length / 3)
     .Select(i => data[(i * 3)..(i * 3 + 3)])
-    .Select(group => group[0].Intersect(group[1]).Intersect(group[2]).First()).ToArray();
+    .Select(group => group.Aggregate<IEnumerable<char>>((previous, next) => previous.Intersect(next)).First()); 
 
 int Value(char c) => c > 'Z' ? c - 'a' + 1 : c - 'A' + 27;
 
