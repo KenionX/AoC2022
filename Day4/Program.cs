a302cf978bc4b46f140bb4088fa9b315880ff3bf
Kenion <zerrayX@gmail.com>
2022-12-04 09:32:18 +0100

    Day4

---
+++ Day4/Program.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable PossibleMultipleEnumeration
+var pairs = File.ReadLines("input.txt").Select(i => i.Split(',').Select(elf =>
+{
+    var range = elf.Split('-').Select(int.Parse).ToArray();
+    return Enumerable.Range(range[0], range[1] - range[0] + 1);
+}).ToArray());
+
+var validPairs = pairs.Where(pair =>
+{
+    var intersected = pair[0].Intersect(pair[1]);
+    return intersected.Count() == Math.Min(pair[0].Count(), pair[1].Count());
+});
+
+//part2
+var validPairs2 = pairs.Where(pair =>
+{
+    var intersected = pair[0].Intersect(pair[1]);
+    return intersected.Any();
+});
+
+Console.WriteLine($"Answer1: {validPairs.Count()}");
+Console.WriteLine($"Answer2: {validPairs2.Count()}");
